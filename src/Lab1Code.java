852564997e53732e4aaceae7a0f0ddca0ed3a6db
Aiden Kiss <aidenk.7103@gmail.com>
2023-09-13 21:22:01 -0400

    made some progress, did the death and population and the summing methods

--- src/Lab1Code.java
+++ src/Lab1Code.java
@@ -2,7 +2,7 @@ public class Lab1Code {
     public static void main(String[] args) {
         int[][] matrix = new int[10][10];
 //        Random rand = new Random();
-        int[][]sumMatrix = new int[10][10];
+        int[][]sum = new int[10][10];
 
         //for loop for the rows
         for (int row = 0; row < 10; row++){
@@ -24,50 +24,50 @@ public class Lab1Code {
             for (int col = 0; col < 10; col++){
                 //for summing the middle of the array
                 if(row > 0 && row < 9 && col >0 && col < 9){
-                    sumMatrix[row][col] = (matrix[row-1][col-1])+(matrix[row-1][col])+(matrix[row-1][col+1])
+                    sum[row][col] = (matrix[row-1][col-1])+(matrix[row-1][col])+(matrix[row-1][col+1])
                         +(matrix[row][col-1]) +(matrix[row][col+1])+
                         (matrix[row+1][col-1])+(matrix[row+1][col])+(matrix[row+1][col+1]);
                 }
                 //for summing the corners of the array
                 //top left corner
                 if(row==0 && col == 0){
-                    sumMatrix[row][col] = (matrix[row][col+1])+(matrix[row+1][col])+(matrix[row+1][col+1]);
+                    sum[row][col] = (matrix[row][col+1])+(matrix[row+1][col])+(matrix[row+1][col+1]);
                 }
                 //top right corner
                 if(row==0 && col == 9){
-                    sumMatrix[row][col] = (matrix[row][col-1])+(matrix[row+1][col])+(matrix[row+1][col-1]);
+                    sum[row][col] = (matrix[row][col-1])+(matrix[row+1][col])+(matrix[row+1][col-1]);
 
                 }
                 //bottom left corner
                 if(row==9 && col == 0){
-                    sumMatrix[row][col] = (matrix[row][col+1])+(matrix[row-1][col])+(matrix[row-1][col+1]);
+                    sum[row][col] = (matrix[row][col+1])+(matrix[row-1][col])+(matrix[row-1][col+1]);
 
                 }
                 //bottom right corner
                 if(row==9 && col == 9){
-                    sumMatrix[row][col] = (matrix[row][col-1])+(matrix[row-1][col])+(matrix[row-1][col-1]);
+                    sum[row][col]= (matrix[row][col-1])+(matrix[row-1][col])+(matrix[row-1][col-1]);
                 }
                 //for summing the outer rows
                 //top row
                 if (row==0 && (col>0 && col <9 )) {
-                    sumMatrix[row][col] = (matrix[row][col - 1]) + (matrix[row][col + 1]) +
+                    sum[row][col]= (matrix[row][col - 1]) + (matrix[row][col + 1]) +
                         (matrix[row + 1][col - 1]) + (matrix[row + 1][col]) + (matrix[row + 1][col + 1]);
                 }
                 //bottom row
                 if (row==9 && (col>0 && col <9 )){
-                    sumMatrix[row][col] = (matrix[row][col-1]) + (matrix[row][col+1])
+                    sum[row][col]= (matrix[row][col-1]) + (matrix[row][col+1])
                         +(matrix[row-1][col-1])+(matrix[row-1][col])+(matrix[row-1][col+1]);
 
                 }
                 //for summing the outer columns
                 //left column
                 if (col==0 && (row >0 && row < 9)) {
-                    sumMatrix[row][col] = (matrix[row][col+1])+(matrix[row-1][col])+(matrix[row+1][col])+
+                    sum[row][col] = (matrix[row][col+1])+(matrix[row-1][col])+(matrix[row+1][col])+
                         (matrix[row-1][col+1])+(matrix[row+1][col+1]);
                 }
                 //right column
                 if (col==9 && (row >0 && row < 9)) {
-                    sumMatrix[row][col] = (matrix[row][col-1])+(matrix[row-1][col])+(matrix[row+1][col])+
+                    sum[row][col]= (matrix[row][col-1])+(matrix[row-1][col])+(matrix[row+1][col])+
                         (matrix[row+1][col-1])+(matrix[row-1][col-1]);
                 }
             }
@@ -82,7 +82,7 @@ public class Lab1Code {
             //nested for loop for the columns
             for (int col = 0; col < 10; col++){
                 //determines if the row is even or odd
-                System.out.print(sumMatrix[row][col] + "\t");
+                System.out.print(sum + "\t");
             }
         }
     }
