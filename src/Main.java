e6384ec13714d4c56f4c5b006f3eb6e477e74774
Aiden Kiss <aidenk.7103@gmail.com>
2023-09-13 21:22:01 -0400

    made some progress, did the death and population and the summing methods

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,106 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //can u see this
-        //Steven Test
-        //method
+
+        int [][] generation = new int[10][10];
+        int [][] futureGen;
+
+        Scanner scan = new Scanner(System.in);
+
+        int rounds;
+
+        System.out.println("enter the number of rounds");
+        rounds = scan.nextInt();
+
+        System.out.println("You selected "+ rounds + "rounds");
 
 
-        //second thing
+
+
     }
+
+    static int[][] nextGen (int[][] matrix){
+        for (int i = 0; i < 10; i++ ){
+            for (int j = 0; j< 10; j++){
+                //new cell is born
+                if (matrix[i][j] == 0 && sumMatrix(matrix) == 3){
+                    matrix[i][j] = 1;
+                //Cell dies of over population
+                } else if(matrix[i][j] == 1 && sumMatrix(matrix) > 3){
+                    matrix[i][j] = 0;
+                //Cell is lonely and dies
+                } else if (matrix[i][j] == 1 && sumMatrix(matrix) < 2) {
+                    matrix[i][j] = 0;
+                //Stays the same
+                } else {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return matrix;
+    }
+
+    static int sumMatrix(int[][] matrix){
+        int sum = 0;
+
+        for (int row = 0; row < 10; row++){
+            //nested for loop for the columns
+            for (int col = 0; col < 10; col++){
+                //for summing the middle of the array
+                if(row > 0 && row < 9 && col >0 && col < 9){
+                    sum = (matrix[row-1][col-1])+(matrix[row-1][col])+(matrix[row-1][col+1])
+                        +(matrix[row][col-1]) +(matrix[row][col+1])+
+                        (matrix[row+1][col-1])+(matrix[row+1][col])+(matrix[row+1][col+1]);
+                }
+                //for summing the corners of the array
+                //top left corner
+                if(row==0 && col == 0){
+                    sum = (matrix[row][col+1])+(matrix[row+1][col])+(matrix[row+1][col+1]);
+                }
+                //top right corner
+                if(row==0 && col == 9){
+                    sum = (matrix[row][col-1])+(matrix[row+1][col])+(matrix[row+1][col-1]);
+                }
+
+                //bottom left corner
+                if(row==9 && col == 0){
+                    sum = (matrix[row][col+1])+(matrix[row-1][col])+(matrix[row-1][col+1]);
+                }
+
+                //bottom right corner
+                if(row==9 && col == 9){
+                    sum = (matrix[row][col-1])+(matrix[row-1][col])+(matrix[row-1][col-1]);
+                }
+
+                //for summing the outer rows
+                //top row
+                if (row==0 && (col>0 && col <9 )) {
+                    sum = (matrix[row][col - 1]) + (matrix[row][col + 1]) +
+                        (matrix[row + 1][col - 1]) + (matrix[row + 1][col]) + (matrix[row + 1][col + 1]);
+                }
+
+                //bottom row
+                if (row==9 && (col>0 && col <9 )){
+                    sum = (matrix[row][col-1]) + (matrix[row][col+1])
+                        +(matrix[row-1][col-1])+(matrix[row-1][col])+(matrix[row-1][col+1]);
+                }
+
+                //for summing the outer columns
+                //left column
+                if (col==0 && (row >0 && row < 9)) {
+                    sum = (matrix[row][col+1])+(matrix[row-1][col])+(matrix[row+1][col])+
+                        (matrix[row-1][col+1])+(matrix[row+1][col+1]);
+                }
+
+                //right column
+                if (col==9 && (row >0 && row < 9)) {
+                    sum = (matrix[row][col-1])+(matrix[row-1][col])+(matrix[row+1][col])+
+                        (matrix[row+1][col-1])+(matrix[row-1][col-1]);
+                }
+            }
+        }
+        return sum;
+    }
+
 }
